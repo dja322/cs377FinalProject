2b3e309786715b1c0ef5ce58df3efe97494f4fc0
dja322 <dja322@nau.edu>
2025-04-27 15:36:33 -0700

    Add basic favorite activity class

--- app/src/main/java/com/app/cs377final/ui/FavoritesActivity.kt
+++ app/src/main/java/com/app/cs377final/ui/FavoritesActivity.kt
@@ -1,4 +1,24 @@
 package com.app.cs377final.ui
 
-class FavoritesActivity {
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.app.cs377final.R
+
+class FavoritesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_favorite)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+
+    }
 }
